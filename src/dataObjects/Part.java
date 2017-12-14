192a79d35ff4a51c99312b3a28cc2bfd82ab0cfd
Abhishek Gandhi <abhi08638@users.noreply.github.com>
2017-12-13 22:00:00 -0500

    Create Part.java

---
+++ src/dataObjects/Part.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataObjects;
+
+/**
+ *
+ * @author Abhi
+ */
+public abstract class Part {
+    
+    private int partId;
+    private String name;
+    private double price;
+    private int inStock;
+    private int min;
+    private int max;
+
+    public int getPartId() {
+        return partId;
+    }
+
+    public void setPartId(int partId) {
+        this.partId = partId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getInStock() {
+        return inStock;
+    }
+
+    public void setInStock(int inStock) {
+        this.inStock = inStock;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+    
+}
